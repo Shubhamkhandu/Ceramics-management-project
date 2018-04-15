2b183e80033e0b4f1ea394e63527cfd52bf187a3
shubham <shubhamkhandelwal18@gmail.com>
2018-04-15 14:46:57 +0530

    local changes

--- ceramics/admin/templates/header.php
+++ ceramics/admin/templates/header.php
@@ -2,14 +2,14 @@
 	if (isset($_SESSION["userid"])) {
 	?>
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<a class="navbar-brand" href="http://localhost/ceramics/admin/dashboard.php">Vijay Ceramics</a>
+		<a class="navbar-brand" href="/dashboard.php">Vijay Ceramics</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="collapse navbar-collapse nav navbar-nav navbar-right"" id="navbarNav">
 			<ul class="navbar-nav">
 				<li class="nav-item active">
-				<a class="nav-link" href="http://localhost/ceramics/admin/dashboard.php"><i class="fa fa-home">&nbsp;</i>Home <span class="sr-only">(current)</span></a>
+				<a class="nav-link" href="/dashboard.php"><i class="fa fa-home">&nbsp;</i>Home <span class="sr-only">(current)</span></a>
 				</li>
 				<li class="nav-item active navbar-right">
 				<a class="nav-link" href="logout.php"><i class="fa fa-user">&nbsp;</i>Logout</a>
@@ -24,14 +24,14 @@
 	else {
 ?>
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<a class="navbar-brand" href="http://localhost/ceramics/index.php">Vijay Ceramics</a>
+		<a class="navbar-brand" href="../index.php">Vijay Ceramics</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="collapse navbar-collapse" id="navbarNav">
 			<ul class="navbar-nav">
 				<li class="nav-item active">
-				<a class="nav-link" href="http://localhost/ceramics/index.php"><i class="fa fa-home">&nbsp;</i>Home <span class="sr-only">(current)</span></a>
+				<a class="nav-link" href="../index.php"><i class="fa fa-home">&nbsp;</i>Home <span class="sr-only">(current)</span></a>
 				</li>
                 	</ul>
 
